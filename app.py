d133174a5602c7818e5973812a1e1a998ce2f0ba
Jayasurya M <123261802+Jayasurya5454@users.noreply.github.com>
2024-04-06 22:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import concurrent.futures
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -13,18 +12,20 @@ def extract_transcript(video_id):
         st.error(f"Error extracting transcript: {e}")
         return None
 
-# Function to summarize transcript in chunks
-def summarize_chunk(chunk):
+# Function to summarize transcript
+def summarize_transcript(transcript_text):
     try:
         summarizer = pipeline("summarization")
-        return summarizer(chunk, max_length=1024, min_length=50, length_penalty=2.0, num_beams=4, temperature=0.5)[0]["summary_text"]
+        summary = summarizer(transcript_text, max_length=1024, min_length=50, length_penalty=2.0, num_beams=4, temperature=0.5)
+        return summary[0]["summary_text"]
     except Exception as e:
-        st.error(f"Error summarizing chunk: {e}")
+        st.error(f"Error summarizing transcript: {e}")
         return None
 
 # Main function to run Streamlit app
 def main():
-    st.title("YouTube Video Summarizer by Kaaraalan AI")
+    st.title("YouTube Video Summarizer")
+    st.write("Enter the YouTube video link below:")
 
     video_link = st.text_input("YouTube Video Link", "")
 
@@ -33,15 +34,11 @@ def main():
             st.warning("Please enter a valid YouTube video link.")
         else:
             video_id = video_link.split("v=")[1]
-            with st.spinner("Summarizing..."):
-                transcript_text = extract_transcript(video_id)
-                if transcript_text:
-                    chunk_size = 500
-                    chunks = [transcript_text[i:i + chunk_size] for i in range(0, len(transcript_text), chunk_size)]
-                    with concurrent.futures.ThreadPoolExecutor() as executor:
-                        summaries = list(executor.map(summarize_chunk, chunks))
-                    summary_text = " ".join(summaries)
-                    st.write("Summary:")
+            transcript_text = extract_transcript(video_id)
+            if transcript_text:
+                st.write("Summary:")
+                summary_text = summarize_transcript(transcript_text)
+                if summary_text:
                     st.write(summary_text)
 
 if __name__ == "__main__":
