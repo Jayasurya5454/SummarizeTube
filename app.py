3eb4703a53a312ac73fd85796c35f6077d4a5997
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-04-06 21:57:02 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import concurrent.futures
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -12,20 +13,18 @@ def extract_transcript(video_id):
         st.error(f"Error extracting transcript: {e}")
         return None
 
-# Function to summarize transcript
-def summarize_transcript(transcript_text):
+# Function to summarize transcript in chunks
+def summarize_chunk(chunk):
     try:
         summarizer = pipeline("summarization")
-        summary = summarizer(transcript_text, max_length=1024, min_length=50, length_penalty=2.0, num_beams=4, temperature=0.5)
-        return summary[0]["summary_text"]
+        return summarizer(chunk, max_length=1024, min_length=50, length_penalty=2.0, num_beams=4, temperature=0.5)[0]["summary_text"]
     except Exception as e:
-        st.error(f"Error summarizing transcript: {e}")
+        st.error(f"Error summarizing chunk: {e}")
         return None
 
 # Main function to run Streamlit app
 def main():
-    st.title("YouTube Video Summarizer")
-    st.write("Enter the YouTube video link below:")
+    st.title("YouTube Video Summarizer by Kaaraalan AI")
 
     video_link = st.text_input("YouTube Video Link", "")
 
@@ -34,11 +33,15 @@ def main():
             st.warning("Please enter a valid YouTube video link.")
         else:
             video_id = video_link.split("v=")[1]
-            transcript_text = extract_transcript(video_id)
-            if transcript_text:
-                st.write("Summary:")
-                summary_text = summarize_transcript(transcript_text)
-                if summary_text:
+            with st.spinner("Summarizing..."):
+                transcript_text = extract_transcript(video_id)
+                if transcript_text:
+                    chunk_size = 500
+                    chunks = [transcript_text[i:i + chunk_size] for i in range(0, len(transcript_text), chunk_size)]
+                    with concurrent.futures.ThreadPoolExecutor() as executor:
+                        summaries = list(executor.map(summarize_chunk, chunks))
+                    summary_text = " ".join(summaries)
+                    st.write("Summary:")
                     st.write(summary_text)
 
 if __name__ == "__main__":
