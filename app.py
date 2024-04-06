fba5e8da4187e6c26f8e0c7e18e8fa2a4b7a4a1c
Jayasurya M <123261802+Jayasurya5454@users.noreply.github.com>
2024-04-06 22:31:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
+import time
 
 # Function to extract transcript from YouTube video
 def extract_transcript(video_id):
@@ -9,7 +10,7 @@ def extract_transcript(video_id):
         transcript_text = " ".join([entry["text"] for entry in transcript])
         return transcript_text
     except Exception as e:
-        st.error(f"Error extracting transcript: {e}")
+        st.error(f"❌ Error extracting transcript: {e}")
         return None
 
 # Function to summarize transcript
@@ -19,26 +20,28 @@ def summarize_transcript(transcript_text):
         summary = summarizer(transcript_text, max_length=1024, min_length=50, length_penalty=2.0, num_beams=4, temperature=0.5)
         return summary[0]["summary_text"]
     except Exception as e:
-        st.error(f"Error summarizing transcript: {e}")
+        st.error(f"❌ Error summarizing transcript: {e}")
         return None
 
 # Main function to run Streamlit app
 def main():
-    st.title("YouTube Video Summarizer by Kaaraalan AI ")
+    st.title(" Kaaraalan AI YouTube Video Summarizer")
 
-    video_link = st.text_input("YouTube Video Link", "")
+    video_link = st.text_input("🎥 YouTube Video Link", "")
 
-    if st.button("Summarize"):
+    if st.button("🚀 Summarize"):
         if "youtube.com" not in video_link:
-            st.warning("Please enter a valid YouTube video link.")
+            st.warning("⚠️ Please enter a valid YouTube video link.")
         else:
-            video_id = video_link.split("v=")[1]
-            transcript_text = extract_transcript(video_id)
-            if transcript_text:
-                st.write("Summary:")
-                summary_text = summarize_transcript(transcript_text)
-                if summary_text:
-                    st.write(summary_text)
+            with st.spinner("Summarizing..."):
+                time.sleep(3)  # Simulating some processing time
+                video_id = video_link.split("v=")[1]
+                transcript_text = extract_transcript(video_id)
+                if transcript_text:
+                    st.write("📝")
+                    summary_text = summarize_transcript(transcript_text)
+                    if summary_text:
+                        st.write(summary_text)
 
 if __name__ == "__main__":
     main()
